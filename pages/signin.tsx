de5e887c686ebd91a31da2eb97d13a837faa78e6
xDamneDx <xdamnedx@gmail.com>
2022-07-27 10:47:56 +0300

    Added signing up and redirecting

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Pane, majorScale, Text } from 'evergreen-ui'
-import Logo from '../components/logo'
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Pane, majorScale, Text } from "evergreen-ui";
+import { signIn, useSession } from "next-auth/client";
 
-import SocialButton from '../components/socialButton'
+// Components:
+import Logo from "../components/logo";
+import SocialButton from "../components/socialButton";
 
 const Signin = () => {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  // If a user is logged it, never show them this page,
+  // go straight to app:
+  useEffect(() => {
+    if (session) {
+      router.push("/app");
+    }
+  }, [session, router]);
+
   return (
     <Pane height="100vh" width="100vw" display="flex">
       <Pane
@@ -37,11 +51,11 @@ const Signin = () => {
         paddingX={majorScale(7)}
       >
         <Pane width="100%" textAlign="center">
-          <SocialButton type="github" onClick={() => {}} />
+          <SocialButton type="github" onClick={() => signIn("github")} />
         </Pane>
       </Pane>
     </Pane>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
